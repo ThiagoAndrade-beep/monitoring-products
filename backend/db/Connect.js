591c7508a12120886dd083a783c1695f366f95d3
Thiago Andrade <thiagopollinho@gmail.com>
2026-02-06 12:27:31 -0300

    uri env mongo

--- backend/db/Connect.js
+++ backend/db/Connect.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
 
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASSWORD
+const dbUri = process.env.MONGO_URI
 
 
 async function connect() {
     try {
-        await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.yicwex9.mongodb.net/monitoringProducts?retryWrites=true&w=majority&appName=Cluster0`)
+        await mongoose.connect(dbUri)
         console.log("Banco conectado!")
     } catch (error) {
         console.log("Erro ao conectar com o banco:", error)
