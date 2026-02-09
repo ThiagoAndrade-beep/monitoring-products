b6b431ddc2d2d03d09aa5bad721a291ab5a49d19
Thiago Andrade <thiagopollinho@gmail.com>
2026-02-09 20:40:57 -0300

    testando cron

--- backend/jobs/priceMonitor.job.js
+++ backend/jobs/priceMonitor.job.js
@@ -27,7 +27,7 @@ function startPriceMonitorJob() {
                 const [data] = await takingData(product.link)
                 const currentPrice = normalizePrice(data.price);
                 const previousPrice = product.lastPrice;
-                const fakePrice = currentPrice - 100;
+                const fakePrice = currentPrice - 30;
 
                 if (fakePrice !== previousPrice) {
 
