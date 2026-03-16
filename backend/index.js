c0ddfe089407316f4c361422afaa832231c8aaad
Thiago Andrade <thiagopollinho@gmail.com>
2026-03-16 15:20:32 -0300

    aumentando timeout da navegação da url

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ async function takingData(url) {
         await page.setUserAgent( 
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
         )
-        await page.goto(url, { waitUntil: "domcontentloaded"})
+        await page.goto(url, { waitUntil: "domcontentloaded", timeout: 60000})
 
         const nameSelector = "#productTitle"
         await page.waitForSelector(nameSelector, { timeout: 60000 })
