16ce68714ade4dc61ba5c5e858fd92d922cee379
Thiago Andrade <thiagopollinho@gmail.com>
2026-08-10 17:09:52 -0300

    teste req

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-const fetchApi = axios.create({
-    baseURL: import.meta.env.VITE_API_URL + "auth",
+const fetchApiDev = axios.create({
+    baseURL: 'http://localhost:3000/auth',
     headers: {
         "Content-Type": "application/json"
     }
 })
 
-export default fetchApi
+export default fetchApiDev
